80b77cb547bc56834ac3de1444b8bf52f8f7d834
XDream8 <ism.frat@hotmail.com>
2022-02-02 21:10:56 +0300

    luakit(theme.lua): better syntax

--- .config/luakit/theme.lua
+++ .config/luakit/theme.lua
@@ -6,28 +6,29 @@ local theme = {}
 
 -- Default settings
 theme.font = "12px FiraCode Nerd Font"
-theme.fg   = "#f8f8f2"
+theme.bg   = "#282A36"
+theme.fg   = "#F8F8F2"
 
 -- Genaral colours
 theme.success_fg = "#50FA7B"
 theme.loaded_fg  = "#33AADD"
-theme.error_fg = "#f8f8f2"
+theme.error_fg = theme.fg
 theme.error_bg = "#FF5555"
 
 -- Warning colours
 theme.warning_fg = "#FF5555"
-theme.warning_bg = "#f8f8f2"
+theme.warning_bg = theme.fg
 
 -- Notification colours
 theme.notif_fg = "#4D4D4D"
-theme.notif_bg = "#f8f8f2"
+theme.notif_bg = theme.fg
 
 -- Menu colours
-theme.menu_fg                   = "#f8f8f2"
-theme.menu_bg                   = "#282A36"
-theme.menu_selected_fg          = "#282A36"
+theme.menu_fg                   = theme.fg
+theme.menu_bg                   = theme.bg
+theme.menu_selected_fg          = theme.bg
 theme.menu_selected_bg          = "#F1FA8C"
-theme.menu_title_bg             = "#f8f8f2"
+theme.menu_title_bg             = "#4D4D4D"
 theme.menu_primary_title_fg     = "#FF5555"
 theme.menu_secondary_title_fg   = "#4D4D4D"
 
@@ -39,34 +40,34 @@ theme.menu_active_fg = "#5AF78E"
 theme.menu_active_bg = theme.menu_bg
 
 -- Proxy manager
-theme.proxy_active_menu_fg      = '#f8f8f2'
-theme.proxy_active_menu_bg      = '#282A36'
-theme.proxy_inactive_menu_fg    = '#f8f8f2'
+theme.proxy_active_menu_fg      = theme.fg
+theme.proxy_active_menu_bg      = theme.bg
+theme.proxy_inactive_menu_fg    = theme.fg
 theme.proxy_inactive_menu_bg    = '#BFBFBF'
 
 -- Statusbar specific
-theme.sbar_fg         = "#f8f8f2"
-theme.sbar_bg         = "#282A36"
+theme.sbar_fg         = theme.fg
+theme.sbar_bg         = theme.bg
 
 -- Downloadbar specific
-theme.dbar_fg         = "#f8f8f2"
-theme.dbar_bg         = "#282A36"
+theme.dbar_fg         = theme.fg
+theme.dbar_bg         = theme.bg
 theme.dbar_error_fg   = "#FF5555"
 
 -- Input bar specific
-theme.ibar_fg           = "#282A36"
-theme.ibar_bg           = "rgba(0,0,0,0)"
+theme.ibar_fg           = theme.fg
+theme.ibar_bg           = theme.bg
 
 -- Tab label
 theme.tab_fg            = "#BFBFBF"
 theme.tab_bg            = "#4D4D4D"
-theme.tab_hover_bg      = "#282A36"
+theme.tab_hover_bg      = theme.bg
 theme.tab_ntheme        = "#e6e6e6"
-theme.selected_fg       = "#f8f8f2"
-theme.selected_bg       = "#282A36"
-theme.selected_ntheme   = "#282A36"
+theme.selected_fg       = theme.fg
+theme.selected_bg       = theme.bg
+theme.selected_ntheme   = theme.bg
 theme.loading_fg        = "#9AEDFE"
-theme.loading_bg        = "#282A36"
+theme.loading_bg        = theme.bg
 
 theme.selected_private_tab_bg = "#CAA9FA"
 theme.private_tab_bg    = "#BD93F9"
@@ -76,20 +77,20 @@ theme.trust_fg          = "#5AF78E"
 theme.notrust_fg        = "#FF6E67"
 
 -- Follow mode hints
-theme.hint_font = "10px monospace, courier, sans-serif"
-theme.hint_fg = "#f8f8f2"
-theme.hint_bg = "#282A36"
+theme.hint_font = "10px FiraCode Nerd Font Mono, courier, sans-serif"
+theme.hint_fg = theme.fg
+theme.hint_bg = theme.bg
 theme.hint_border = "1px dashed #000"
 theme.hint_opacity = "0.3"
 theme.hint_overlay_bg = "rgba(255,255,153,0.3)"
-theme.hint_overlay_border = "1px dotted #000"
+theme.hint_overlay_border = "1px dotted #282A36"
 theme.hint_overlay_selected_bg = "rgba(0,255,0,0.3)"
 theme.hint_overlay_selected_border = theme.hint_overlay_border
 
 -- General colour pairings
-theme.ok = { fg = "#f8f8f2", bg = "#282A36" }
-theme.warn = { fg = "#FF5555", bg = "#282A36" }
-theme.error = { fg = "#f8f8f2", bg = "#FF5555" }
+theme.ok = { fg = theme.fg, bg = theme.bg }
+theme.warn = { fg = "#FF5555", bg = theme.bg }
+theme.error = { fg = theme.fg, bg = "#FF5555" }
 
 -- Gopher page style (override defaults)
 theme.gopher_light = { bg = "#E8E8E8", fg = "#17181C", link = "#03678D" }
